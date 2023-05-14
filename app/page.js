511dc771e67ec2f46a039d1cb5bd29cd23fa248e
AbdullahMufti <abdullahmufty@gmail.com>
2023-05-14 10:48:22 +0500

    second commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,216 @@
-import Image from 'next/image'
-
+"use client";
+import { useState } from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import TField from "@/components/TField";
 export default function Home() {
+  const [Phone, setPhone] = useState("");
+  const [Result, setResult] = useState(false);
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("name is required"),
+    email: Yup.string().required("email is required"),
+    address: Yup.string().required("address is required"),
+    city: Yup.string().required("city is required"),
+    state: Yup.string().required("state is required"),
+    education: Yup.string().required("education is required"),
+    gender: Yup.string().required("gender is required"),
+    age: Yup.string().required("age is required"),
+  });
+
+  const initialValues = {
+    name: "",
+    email: "",
+    Phone: "",
+    address: "",
+    city: "",
+    state: "",
+    gender: "",
+    education: "",
+    age: "",
+  };
+
+  function SaveToDB(kQuery) {
+    var data = new FormData();
+    data.append("q", kQuery);
+    let xmlhttp;
+
+    try {
+      if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+      } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          alert(this.response);
+          setResult(true);
+        }
+      };
+
+      xmlhttp.open("POST", "openmicreg.php", true);
+      xmlhttp.send(data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  const onSubmit = (values) => {
+    if (Phone === "") {
+      alert("please add your whatsapp number");
+    } else {
+      const kQuery =
+        "('" +
+        values.name +
+        "','" +
+        values.email +
+        "','" +
+        Phone +
+        "','" +
+        values.address +
+        "','" +
+        values.city +
+        "','" +
+        values.state +
+        "','" +
+        values.gender +
+        "','" +
+        values.education +
+        "','" +
+        values.age +
+        "')";
+
+      SaveToDB(kQuery);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <h1 className="text-4xl text-center">Society</h1>
+      <h2 className="text-2xl text-center">Openmic Registration Form</h2>
+      {!Result && (
+        <div className="w-full flex flex-wrap-reverse">
+          <div className="w-full sm:w-10/12 md:w-9/12 mx-auto">
+            <h2>Openmic Registration</h2>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+              {({ errors, touched }) => (
+                <Form>
+                  <div className="flex flex-wrap">
+                    <TField
+                      name="name"
+                      inputlabel="Full Name"
+                      placeholder="Please Enter your Name"
+                      CName="w-full md:w-1/3 my-2 px-5"
+                      type="text"
+                      required
+                    />
+                    <TField
+                      name="email"
+                      inputlabel="Email Address"
+                      placeholder="Please Enter your Email ID"
+                      CName="w-full md:w-1/3 my-2 px-5"
+                      type="email"
+                      required
+                    />
+                    <div className="w-full md:w-1/3 my-2 px-5">
+                      <label
+                        className="flex h-10 w-full items-end"
+                        htmlFor="Phone"
+                      >
+                        Whatsapp Number *
+                      </label>
+                      <PhoneInput
+                        id="Phone"
+                        name="Phone"
+                        className="flex w-full md:w-1/3  rounded-md  bg-transparent px-3  text-xs ring-offset-background  file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                        value={Phone}
+                        onChange={(phone) => setPhone(phone)}
+                        required
+                      />
+                    </div>
+
+                    <div className="w-1/2 md:w-1/4 my-2 px-5">
+                      <label
+                        htmlFor="gender"
+                        className="flex h-10 w-full items-end"
+                      >
+                        {" "}
+                        Select your gender
+                      </label>
+                      <Field
+                        as="select"
+                        name="gender"
+                        className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                        required
+                      >
+                        <option value="" disabled defaultValue>
+                          Gender
+                        </option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                      </Field>
+                      <ErrorMessage name="gender" component="div" />
+                    </div>
+                    <TField
+                      name="age"
+                      inputlabel="Age"
+                      placeholder="Age"
+                      CName="w-1/2 md:w-1/4 my-2 px-5"
+                      type="text"
+                      required
+                    />
+                    <TField
+                      name="education"
+                      inputlabel="Education"
+                      placeholder="Education"
+                      CName="w-full md:w-1/2 my-2 px-5"
+                      type="text"
+                      required
+                    />
+
+                    <TField
+                      name="address"
+                      inputlabel="Address"
+                      placeholder="Please Enter Address"
+                      CName="w-full md:w-1/2 my-2 px-5"
+                      type="text"
+                      required
+                    />
+                    <TField
+                      name="city"
+                      inputlabel="City"
+                      placeholder="City"
+                      CName="w-1/2 md:w-1/4 my-2 px-5"
+                      type="text"
+                      required
+                    />
+                    <TField
+                      name="state"
+                      inputlabel="State / Province / Region"
+                      placeholder="State / Province / Region"
+                      CName="w-1/2 md:w-1/4 my-2 px-5"
+                      type="text"
+                      required
+                    />
+
+                    <div className="w-full text-center">
+                      <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full mt-5"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+      {Result && <h1>Thank you for Registration</h1>}
+    </>
+  );
 }
